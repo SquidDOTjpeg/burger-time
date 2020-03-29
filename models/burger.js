86f176dd92e26d4357133112f32a25607079ec8d
AnthonyPillow <anthonypillow18@gmail.com>
2020-03-28 19:56:35 -0700

    Finished work on the burger model

--- models/burger.js
+++ models/burger.js
@@ -1,1 +1,23 @@
 var orm = require("../config/orm")
+
+var burger = {
+    all: function (cb) {
+        orm.selectAll("burgers", (res) => {
+            cb(res)
+        })
+    },
+
+    create: function (cols, vals, cb) {
+        orm.insertOne("burgers", cols, vals, (res) => {
+            cb(res)
+        })
+    },
+
+    update: function (objColVals, condition, cb) {
+        orm.updateOne("burgers", objColVals, condition, (res) => {
+            cb(res)
+        })
+    }
+}
+
+module.exports = burger
